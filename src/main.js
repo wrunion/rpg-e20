429004b7b5b78a9715dbab327348136ba946ef67
Andrew Kim and Winter Runion <andrew+winter@>
2020-03-01 14:34:45 -0800

    Test Character and Vampire classes.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { Character } from './character.js';
 import { Vampire } from './vampire.js';
 
-console.log(Character);
-console.log(Vampire);
+// console.log(Character);
+// console.log(Vampire);
+
+let character = new Character("Cherry", 20)
+let vampire2 = new  Vampire('Jojo', 20)
+console.log(vampire2.takeDamage(20));
+console.log(character.selfHeal(2));
+
+console.log(vampire2.sayName());
