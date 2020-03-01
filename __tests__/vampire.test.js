42dc7c3cd4d3237e51dc2c4ad6de93e785a29d24
Andrew Kim and Winter Runion <andrew+winter@>
2020-03-01 10:34:44 -0800

    Refactor test suite to use beforeEach.

--- __tests__/vampire.test.js
+++ __tests__/vampire.test.js
@@ -1,13 +1,18 @@
 import { Vampire } from './../src/vampire';
 
 describe('Vampire', () => {
+    var reusableVampire;
 
-    test('should correctly create a vampire character with a name', () => {
-        let vampire = new Vampire("Vlad");
-        expect(vampire.name).toEqual("Vlad");
+    beforeEach(() => {
+    reusableVampire = new Vampire("Vlad");
     });
-    test('should correctly display hp (health points)', () => {
-        let hp = new Hp("20");
-        expect(vampire.hp).toEqual("20");
-    });
+        test('should correctly create a vampire character with a name', () => {
+        reusableVampire = new Vampire("Vlad");
+        expect(reusableVampire.name).toEqual("Vlad");
+        });
+        test('should correctly display hp (health points)', () => {
+            let hp = new Hp(20);
+    reusableVampire = new Vampire("Vlad");
+    expect(vampire.hp).toEqual(20);
+        });
 });
