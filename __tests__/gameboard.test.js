ab44d480ca67616d7dce33c9a1056146083086bd
Andrew Kim and Winter Runion <example@example.com>
2020-03-03 19:29:45 -0800

    fix rollDice test in gameboard.

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -3,7 +3,7 @@ import { Gameboard } from './../src/gameboard';
 describe ('Gameboard', () => {
     test('has a roll dice function that returns a random number between 1 and 6', () => {
         let gameboard = new Gameboard();
-        expect(rollDice()).toBeGreaterThanOrEqual(1);
-        expect(rollDice()).toBeLessThanOrEqual(6);
+        expect(gameboard.rollDice()).toBeGreaterThanOrEqual(1);
+        expect(gameboard.rollDice()).toBeLessThanOrEqual(6);
     });
 });
