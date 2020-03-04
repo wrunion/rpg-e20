6e02973ad4a09f43a655ccadedad33bb0558ada9
Winter Runion <winterrunion@gmail.com>
2020-03-03 22:57:07 -0800

    Add this.specialEventSquare property to Gameboard class.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,9 @@
 export class Gameboard {
  constructor() {
-  this.redSquare = [2,5,8,12,15,20,23,25,29,35];
-  this.greenSquare = [1,3,7,10,14,17,21,26,30,32];
- }å
+  this.redSquare = [1,4,8,12,15,20,23,25,29,35];
+  this.greenSquare = [2,3,6,9,10,14,17,21,26,30,31];
+  this.specialEventSquares = [5,7,10,16,25,32];
+ }
  rollDice() {
   return(Math.floor(Math.random()*6+1));
  }
