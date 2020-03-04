22c53f8b5f1ded0636c1488a460ff739922a75b8
Winter Runion <winterrunion@gmail.com>
2020-03-03 22:48:40 -0800

    Refactor green and red square functions. All tests still pass.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,26 +1,24 @@
 export class Gameboard {
-    constructor() {
-        this.redSquare = [2,5,8,12,15,20,23,25,29,35];
-        this.greenSquare = [1,3,7,10,14,17,21,26,30,32];
-    }
-    rollDice() {
-        return(Math.floor(Math.random()*6+1));
-    }
-    // takeTurn() {
-    //     let spaces = this.rollDice();
-    // }
+ constructor() {
+  this.redSquare = [2,5,8,12,15,20,23,25,29,35];
+  this.greenSquare = [1,3,7,10,14,17,21,26,30,32];
+ }å
+ rollDice() {
+  return(Math.floor(Math.random()*6+1));
+ }
+ // takeTurn() {
+ //     let spaces = this.rollDice();
+ // }
 
-    isRedSquare(square) {
-        if (this.redSquare.includes(square)) {
-            return true;
-        }
-        return false;
-    }
-    isGreenSquare(square) {
-        if (this.greenSquare.includes(square)) {
-            return true;
-        }
-       return false;
-    }
+ isRedSquare(square) {
+  return this.redSquare.includes(square); 
+ }
+ isGreenSquare(square) {
+  return this.greenSquare.includes(square);
+ }
+
+ isEventSquare(square) {
+
+ }
 
 }
